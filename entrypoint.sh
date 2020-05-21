c9d63e895c641b038885c023ecc2940617feb50d
Norman Moeschter <norman.moeschter@gmail.com>
2020-05-21 11:16:55 +0100

    Fix entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,5 @@ echo "$TLS_CERT" > /data/lnd/tls.cert
 
 echo "Example:"
 echo 'curl -sSL -X GET --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $READ_MAC" https://$LND_URL:8080/v1/channels | jq'
+
+exec $@
