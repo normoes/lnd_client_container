18091ac229ef10edba3b29e86a1d6f9e84d67ebd
Norman Moeschter <norman.moeschter@gmail.com>
2020-09-10 21:39:15 +0100

    Fix bash curl example print.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,12 @@ echo "$TLS_CERT" > /data/lnd/tls.cert
 
 echo "Reference:"
 echo "https://api.lightning.community/#lnd-rest-api-reference"
-
-echo "Example:"
+echo ""
+echo "Example - Check channels:"
 echo 'curl -w "\n" -sSL -X GET --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $READ_MAC" https://$LND_URL:8080/v1/channels | jq'
-echo 'curl -w "\n" -sSL -X POST --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $READ_MAC" https://$LND_URL:8080/v1/unlockwallet -d '{"wallet_password": "base64_passwd"}' | jq'
-echo 'curl -w "\n" -sSL -X POST --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $ADMIN_MAC" https://$LND_URL:8080/v1/invoices -d'{"value_msat": "1500000", "memo": "description of payment"}' | jq'
+echo "Example - Unlock wallet:"
+echo 'curl -w "\n" -sSL -X POST --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $READ_MAC" https://$LND_URL:8080/v1/unlockwallet -d '"'"'{"wallet_password": "base64_passwd"}'"'"' | jq'
+echo "Example - Create invoice:"
+echo 'curl -w "\n" -sSL -X POST --cacert /data/lnd/tls.cert -H "Grpc-Metadata-macaroon: $ADMIN_MAC" https://$LND_URL:8080/v1/invoices -d '"'"'{"value_msat": "1500000", "memo": "description of payment"}'"'"' | jq'
 
 exec $@
